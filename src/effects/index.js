58fe3daff6b36630c081cc522ae7c9aa1d120229
lishudong <lishudong@jusfoun.com>
2018-11-28 13:55:56 +0800

    add peer dependencies

--- src/effects/index.js
+++ src/effects/index.js
@@ -1,3 +1,22 @@
-export { take, takem, put, all, race, call, apply, cps, fork, spawn, join, cancel, select, actionChannel, cancelled, flush, getContext, setContext } from 'redux-saga/effects';
+export {
+  take,
+  takem,
+  put,
+  all,
+  race,
+  call,
+  apply,
+  cps,
+  fork,
+  spawn,
+  join,
+  cancel,
+  select,
+  actionChannel,
+  cancelled,
+  flush,
+  getContext,
+  setContext
+} from "redux-saga/effects";
 
-export { takeEvery, takeLatest, throttle } from 'redux-saga/effects';
+export { takeEvery, takeLatest, throttle } from "redux-saga/effects";
